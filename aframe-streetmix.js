f03859caf464a837e1a63f62b51f27bb0e8dcf7f
kfarr <kieran.farr@gmail.com>
2017-06-24 00:53:43 -0700

    repeat street, combine materials, use new url

--- aframe-streetmix.js
+++ aframe-streetmix.js
@@ -14,7 +14,7 @@ const defaultModelWidthsInMeters = {
 // We need to reduce the size of the model (scale * 0.1) to compensate.
 const voxelScaleFactor = 0.1;
 
-function processSegments(segments) {
+function processSegments(segments, streetElementId) {
   var cumulativeWidthInMeters = 0;
   for (var i = 0; i < segments.length; i++) {
 
@@ -41,8 +41,9 @@ function processSegments(segments) {
     var positionX = cumulativeWidthInMeters - (0.5 * segmentWidthInMeters);
     segmentEl.setAttribute("position", positionX + " 0 0");
     segmentEl.setAttribute("obj-model", "obj", "url(assets/" + segments[i].type + ".obj)");
-    segmentEl.setAttribute("obj-model", "mtl", "url(assets/" + segments[i].type + ".mtl)");
-    document.getElementById("street").appendChild(segmentEl);
+    // segmentEl.setAttribute("obj-model", "mtl", "url(assets/" + segments[i].type + ".mtl)");
+    segmentEl.setAttribute("obj-model", "mtl", "url(assets/magicapalette.mtl)");
+    document.getElementById(streetElementId).appendChild(segmentEl);
   };
 };
 
@@ -52,18 +53,48 @@ var request = new XMLHttpRequest();
 // LOCAL
 // request.open('GET', 'sample.json', true);
 
-// REMOTE WITH REDIRECT - CAUSES CORS ERROR
-// request.open('GET', 'https://streetmix.net/api/v1/streets?namespacedId=3&creatorId=kfarr', true);
+// REMOTE WITH REDIRECT
+request.open('GET', 'https://streetmix.net/api/v1/streets?namespacedId=3&creatorId=kfarr', true);
 
 // DIRECT TO REMOTE FILE
-request.open('GET', 'https://streetmix.net/api/v1/streets/7a633310-e598-11e6-80db-ebe3de713876', true);
+// request.open('GET', 'https://streetmix.net/api/v1/streets/7a633310-e598-11e6-80db-ebe3de713876', true);
 
 request.onload = function() {
   if (this.status >= 200 && this.status < 400) {
     // Connection success
     var streetmixObject = JSON.parse(this.response);
     var streetmixSegments = streetmixObject.data.street.segments;
-    processSegments(streetmixSegments);
+    processSegments(streetmixSegments, "street");
+
+    processSegments(streetmixSegments, "street1");
+    processSegments(streetmixSegments, "street2");
+    processSegments(streetmixSegments, "street3");
+    processSegments(streetmixSegments, "street4");
+    processSegments(streetmixSegments, "street5");
+    processSegments(streetmixSegments, "street6");
+
+    processSegments(streetmixSegments, "street-1");
+    processSegments(streetmixSegments, "street-2");
+    processSegments(streetmixSegments, "street-3");
+    processSegments(streetmixSegments, "street-4");
+
+    // Use JavaScript to repeat the street METHOD ONE
+    // var streetEl = document.getElementById("street");
+    //
+    // streetEl.flushToDOM(true);
+    //
+    // // Copy the element and its child nodes
+    // var streetElCopy = streetEl.cloneNode(true);
+    // streetElCopy.id = "street2";
+    // streetElCopy.setAttribute("position", "0 0 -2");
+    // document.getElementById("streets").appendChild(streetElCopy);
+
+    // Use JavaScript to repeat the street METHOD TWO
+    // var streetEl = document.getElementById("street");
+    // streetEl.flushToDOM();
+    // document.getElementById("street2").innerHTML = streetEl.innerHTML;
+    // // document.getElementById("streets").appendChild(streetElCopy);
+
   } else {
     // We reached our target server, but it returned an error
     console.log("oops - We reached our target server, but it returned an error");
