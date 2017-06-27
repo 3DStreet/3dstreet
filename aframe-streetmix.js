b5679407613750f20d216459ca6b1265195c321c
kfarr <kieran.farr@gmail.com>
2017-06-27 00:53:32 -0700

    add streetmix URL conversion helper functions

--- aframe-streetmix.js
+++ aframe-streetmix.js
@@ -125,15 +125,12 @@ function initStreet() {
   // Is there a URL in the URL HASH?
   var streetURL = location.hash.substring(1);
   console.log("hash check: " + streetURL);
-
   if (!streetURL) {
     // LOCAL
     // var streetURL = 'sample.json';
-
-    // REMOTE WITH REDIRECT
+    // REMOTE WITH REDIRECT:
     var streetURL = 'https://streetmix.net/api/v1/streets?namespacedId=3&creatorId=kfarr';
-
-    // DIRECT TO REMOTE FILE
+    // DIRECT TO REMOTE FILE:
     // var streetURL = 'https://streetmix.net/api/v1/streets/7a633310-e598-11e6-80db-ebe3de713876';
   };
 
@@ -160,3 +157,33 @@ function locationHashChanged() {
   };
 }
 window.onhashchange = locationHashChanged;
+
+function streetmixUserToAPI(userURL) {
+  // this takes in a user facing Streetmix.net URL like https://streetmix.net/kfarr/3/a-frame-city-builder-street-only
+  // and turns it into the API redirect URL like https://streetmix.net/api/v1/streets?namespacedId=3&creatorId=kfarr
+  var pathArray = new URL(userURL).pathname.split( '/' );
+  creatorId = pathArray[1];
+  namespacedId = pathArray[2];
+  return "https://streetmix.net/api/v1/streets?namespacedId=" + namespacedId + "&creatorId=" + creatorId;
+};
+
+function streetmixAPIToUser(APIURL) {
+  // this takes in a Streetmix.net API redirect URL like https://streetmix.net/api/v1/streets?namespacedId=3&creatorId=kfarr
+  // and turns it into the user facing friendly Streetmix.net URL like https://streetmix.net/kfarr/3/a-frame-city-builder-street-only
+
+  // modified from: https://stackoverflow.com/questions/2090551/parse-query-string-in-javascript
+  function getQueryVariable(queryString, variable) {
+    var vars = queryString.split('&');
+    for (var i = 0; i < vars.length; i++) {
+      var pair = vars[i].split('=');
+      if (decodeURIComponent(pair[0]) == variable) {
+        return decodeURIComponent(pair[1]);
+      }
+    }
+    console.log('Query variable %s not found', variable);
+  }
+  var queryString = new URL(APIURL).search.substring(1);
+  var namespacedId = getQueryVariable(queryString, "namespacedId");
+  var creatorId = getQueryVariable(queryString, "creatorId");
+  return "https://streetmix.net/" + creatorId + "/" + namespacedId;
+};
