16441606c6c07cbb9b00d333370b741ea6616013
Kieran Farr <kieran.farr@gmail.com>
2020-05-12 09:19:16 -0700

    basic testing v1

---
+++ test/test.js
@@ -0,0 +1,15 @@
+const rewire = require('rewire');
+const assert = require('assert');
+
+const app = rewire('../src/streetmix-utils');
+const streetmixUserToAPI = app.__get__('streetmixUserToAPI'); 
+
+describe('StreetmixUtils', function() {
+  describe('#streetmixUserToAPI()', function() {
+    it('should return API redirect URL when given user facing URL', function() {
+      assert.equal(
+          streetmixUserToAPI("https://streetmix.net/kfarr/3/a-frame-city-builder-street-only"),
+          "https://streetmix.net/api/v1/streets?namespacedId=3&creatorId=kfarr");
+    });
+  });
+});
