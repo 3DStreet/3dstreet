828653b47175de7e2bea541384c9aa42b0f3ddcd
Kieran Farr <kieran.farr@gmail.com>
2020-05-12 11:07:33 -0700

    add non creator id test for streetmixUserToAPI

--- test/test.js
+++ test/test.js
@@ -8,10 +8,16 @@ const streetmixUserToAPI = app.__get__('streetmixUserToAPI');
 
 describe('StreetmixUtils', function () {
   describe('#streetmixUserToAPI()', function () {
-    it('should return API redirect URL when given user facing URL', function () {
+    it('should return API redirect URL when given user facing URL WITH a creator ID', function () {
       assert.strictEqual(
         streetmixUserToAPI('https://streetmix.net/kfarr/3/a-frame-city-builder-street-only'),
         'https://streetmix.net/api/v1/streets?namespacedId=3&creatorId=kfarr');
+    });
+
+    it('should return API redirect URL when given user facing URL WITHOUT a creator ID', function () {
+      assert.strictEqual(
+        streetmixUserToAPI('https://streetmix.net/-/3/a-frame-city-builder-street-only'),
+        'https://streetmix.net/api/v1/streets?namespacedId=3');
     });
   });
 });
