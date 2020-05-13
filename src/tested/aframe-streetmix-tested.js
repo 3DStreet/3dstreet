f8c5c9cb993bc815c8b9d1568cdeb5ab50d8a059
Kieran Farr <kieran.farr@gmail.com>
2020-05-13 09:41:13 -0700

    adding tested isSidewalk
    Co-Authored-By: Harald Reingruber <haraldreingruber@users.noreply.github.com>

--- src/tested/aframe-streetmix-tested.js
+++ src/tested/aframe-streetmix-tested.js
@@ -1,8 +1,6 @@
-
-function isSidewalk (string) {
-    // https://streetmix.net/api/v1/streets/3f1a9810-0a8f-11ea-adff-7fe273b63f1d
+function isSidewalk (string) { // eslint-disable-line no-unused-vars
+  // https://streetmix.net/api/v1/streets/3f1a9810-0a8f-11ea-adff-7fe273b63f1d
   //  return if string sidewalk* or "scooter-drop-zone", bikeshare, flex-zone-curb, transit-shelter
-    const sidewalkList = ['scooter-drop-zone', 'bikeshare', 'flex-zone-curb', 'transit-shelter'];
-    return string.substring(0, 8) == 'sidewalk' || sidewalkList.includes(string);
-  }
-  
+  const sidewalkList = ['scooter-drop-zone', 'bikeshare', 'flex-zone-curb', 'transit-shelter'];
+  return string.startsWith('sidewalk') || sidewalkList.includes(string);
+}
