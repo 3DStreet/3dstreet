fde37139ade286fe7b2b7e9bfcf707a3d585b1aa
Kieran Farr <kieran.farr@gmail.com>
2020-05-13 09:22:21 -0700

    split out first streetmix3d function for testing
    Co-Authored-By: Harald Reingruber <haraldreingruber@users.noreply.github.com>

--- src/aframe-streetmix.js
+++ src/aframe-streetmix.js
@@ -38,13 +38,6 @@ const defaultModelWidthsInMeters = {
 };
 /* eslint-enable quote-props */
 
-function isSidewalk (string) {
-  // https://streetmix.net/api/v1/streets/3f1a9810-0a8f-11ea-adff-7fe273b63f1d
-//  return if string sidewalk* or "scooter-drop-zone", bikeshare, flex-zone-curb, transit-shelter
-  const sidewalkList = ['scooter-drop-zone', 'bikeshare', 'flex-zone-curb', 'transit-shelter'];
-  return string.substring(0, 8) == 'sidewalk' || sidewalkList.includes(string);
-}
-
 function cloneMixin ({ objectMixinId = '', parentId = '', step = 15, radius = 60, rotation = '0 0 0', positionXYString = '0 0', randomY = false }) {
   for (var j = (radius * -1); j <= radius; j = j + step) {
     var placedObjectEl = document.createElement('a-entity');
