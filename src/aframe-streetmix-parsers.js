d0d6b42860f5764f76ffeb9885227f4f03e28411
srothst1 <samrothstein98@gmail.com>
2021-12-24 19:14:52 -0800

    fixed lint issues

--- src/aframe-streetmix-parsers.js
+++ src/aframe-streetmix-parsers.js
@@ -642,7 +642,7 @@ function processSegments (segments, showStriping, length) {
     } else if (segments[i].type === 'outdoor-dining') {
       groundMixinId = (variantList[1] === 'road') ? 'drive-lane' : 'sidewalk';
       segmentParentEl.append(createOutdoorDining(variantList, positionX));
-    } else if (segments[i].type === 'parklet'){
+    } else if (segments[i].type === 'parklet') {
       groundMixinId = 'drive-lane';
       segmentParentEl.append(createParkletElement(positionX, variantList));
     } else if (segments[i].type === 'bikeshare') {
