9d7188287ef209ad80c7f4006226f57f5877dfa3
Kieran Farr <kieran.farr@gmail.com>
2022-05-18 20:05:22 -0700

    removed test logging

--- src/tested/create-from-json-utils-tested.js
+++ src/tested/create-from-json-utils-tested.js
@@ -5,7 +5,6 @@ function createElementFromObject (object = {}) { // eslint-disable-line no-unuse
   delete object.tag;
   for (const [key, value] of Object.entries(object)) {
     if (key === 'child') {
-      console.log('child', value, value.tag);
       var childEl = document.createElement(value.tag);
       for (const [childKey, childValue] of Object.entries(value)) {
         childEl.setAttribute(childKey, childValue);
