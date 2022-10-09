490ba8134a54ddb674ef5730fab809039679d9d3
gnana sai reddy <gnanasaireddy1998@gmail.com>
2022-10-09 22:01:04 +0530

    test

--- src/tested/vehicle-wheel-animation-test.js
+++ src/tested/vehicle-wheel-animation-test.js
@@ -28,36 +28,38 @@ AFRAME.registerComponent('wheel', {
       self.main_bone = vehicle.getObjectByName('main_bone');
     });
   },
-  tick: function () {
+  tick: function (t,dt) {
     const speed = this.data.speed;
     const wheelDiameter = this.data.wheelDiameter;
 
     const dist = Math.PI * wheelDiameter;
     const distx = speed/1000;
-    const t = (distx / dist) * (2 * Math.PI);
+    const r = (distx / dist) * (2 * Math.PI);
+
+    // console.log(t);
 
     if (this.main_bone) {
-      this.main_bone.position.z += distx;
+      this.main_bone.translateY(distx);
     }
     if (this.wheel_F_L) {
-      this.wheel_F_L.rotateY(t);
+      this.wheel_F_L.rotateY(r);
     }
     if (this.wheel_F_R) {
-      this.wheel_F_R.rotateY(t);
+      this.wheel_F_R.rotateY(r);
     }
     if (this.wheel_B_L) {
-      this.wheel_B_L.rotateY(t);
+      this.wheel_B_L.rotateY(r);
     }
 
     if (this.wheel_B_L_2) {
-      this.wheel_B_L_2.rotateY(t);
+      this.wheel_B_L_2.rotateY(r);
     }
 
     if (this.wheel_B_R_2) {
-      this.wheel_B_R_2.rotateY(t);
+      this.wheel_B_R_2.rotateY(r);
     }
     if (this.wheel_B_R) {
-      this.wheel_B_R.rotateY(t);
+      this.wheel_B_R.rotateY(r);
     }
   }
 });
