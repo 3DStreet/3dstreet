877ef3883167fc6879c2d55249de28d0ed6c57dc
Alexander Goryushkin <aleotu@gmail.com>
2022-10-18 22:30:46 -0300

    add createEntities function

--- src/json-utils.js
+++ src/json-utils.js
@@ -50,3 +50,52 @@ function getAttributes(entity) {
   }
   return elemObj;
 }
+
+function createEntities(EntitiesJSON) {
+  const entitiesData = EntitiesJSON.data;
+
+  // trick to create nodeList with entities to return as result
+  const entityElements = document.createElement('a-entity');
+
+  for (let entityData of entitiesData) {
+    entityElements.appendChild(createEntity(entityData))
+  }
+
+  return entityElements;
+}
+
+/*
+Add a new entity with a list of components and children (if exists)
+ * @param {object} entityData Entity definition to add:
+ *   {element: 'a-entity', components: {geometry: 'primitive:box'}}
+ * @param {Element} parentEl the parent element to which the Entity will be added
+ * @return {Element} Entity created
+*/
+function createEntity(entityData, parentEl) {
+  const entity = document.createElement(entityData.element);
+
+  // load default attributes
+  for (let attr in entityData.components) {
+    entity.setAttribute(attr, entityData.components[attr]);
+  }
+
+  if (entityData.id) {
+    entity.setAttribute("id", entityData.id);
+  }
+  if (entityData.class) {
+    entity.setAttribute("className", entityData.class);
+  }
+
+  if (parentEl) {
+    parentEl.appendChild(entity);
+  }
+
+  if (entityData.children) {
+    let childrenEntities = entityData.children;
+    childrenEntities.forEach(childEntityData => {
+      createEntity(childEntityData, entity);
+    });
+  }
+
+  return entity;
+}
