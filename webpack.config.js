ca8165eafb8218688871989ff9776062f07cb8f6
Kieran Farr <kieran.farr@gmail.com>
2023-08-03 12:30:47 -0400

    do not create license.txt during webpack build
    the license.txt did not reflect actual licenses of the project. Not sure why it just started happening now. docs for future reference:
    https://github.com/webpack-contrib/terser-webpack-plugin/issues/383
    https://github.com/webpack-contrib/terser-webpack-plugin/issues/229#issuecomment-761294644

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const TerserPlugin = require('terser-webpack-plugin');
 
 module.exports = {
   entry: './src/index.js',
@@ -11,5 +12,8 @@ module.exports = {
     rules: [
       { test: /\.js$/, loader: 'babel-loader' }
     ]
+  },
+  optimization: {
+    minimizer: [new TerserPlugin({ extractComments: false })],
   }
 };
