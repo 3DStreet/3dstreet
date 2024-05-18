e9ed7e7b2f877a9c122c462e5795476d47c338fb
Vincent Fretin <vincent.fretin@gmail.com>
2024-05-18 14:25:43 +0200

    no need for file-loader, we can use asset/resource type, although it doesn't seem we have any image import in code

--- webpack.config.js
+++ webpack.config.js
@@ -90,14 +90,10 @@ module.exports = {
       },
       {
         test: /\.(png|jpe?g|gif)$/i,
-        use: [
-          {
-            loader: 'file-loader',
-            options: {
-              name: 'images/[name].[ext]'
-            }
-          }
-        ]
+        type: 'asset/resource',
+        generator: {
+          filename: 'images/[name].[ext]'
+        }
       }
     ]
   }
