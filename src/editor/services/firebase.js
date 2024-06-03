80544c04f93358fe8bf84b817064ad966c7853df
Rostyslav Nahornyi <rostyslav.nahornyi@zade.agency>
2024-06-03 14:04:01 +0300

    feat: firebase emulator

--- src/editor/services/firebase.js
+++ src/editor/services/firebase.js
@@ -1,7 +1,11 @@
 import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 import { getStorage } from 'firebase/storage';
-import { getFirestore } from 'firebase/firestore';
+import { getFirestore, connectFirestoreEmulator } from 'firebase/firestore';
+
+const isLocalhostAndMissingFirebaseEnv =
+  window.location.hostname === 'localhost' &&
+  (!process.env.FIREBASE_PROJECT_ID || !process.env.FIREBASE_API_KEY);
 
 const firebaseConfig = {
   apiKey: process.env.FIREBASE_API_KEY,
@@ -17,5 +21,10 @@ const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const storage = getStorage(app);
 const db = getFirestore(app);
+
+if (isLocalhostAndMissingFirebaseEnv) {
+  console.log('Using Firestore Emulator');
+  connectFirestoreEmulator(db, 'localhost', 8080);
+}
 
 export { auth, storage, db };
