aac811051c1d223fe47ad9160f363d9928b30972
Rahul Gupta <rahulkartgupta@gmail.com>
2024-06-19 09:17:48 -0700

    add posthog auth

--- src/editor/api/auth.js
+++ src/editor/api/auth.js
@@ -1,17 +1,17 @@
 import { GoogleAuthProvider, signInWithPopup } from 'firebase/auth';
 import { auth } from '../services/firebase';
 import { sendMetric } from '../services/ga';
+import posthog from 'posthog-js';
 
 const signIn = async () => {
   try {
-    const {
-      user: {
-        metadata: { creationTime, lastSignInTime }
-      }
-    } = await signInWithPopup(auth, new GoogleAuthProvider());
-
+    const { user } = await signInWithPopup(auth, new GoogleAuthProvider());
+    posthog.identify(user.uid, {
+      email: user.email,
+      name: user.displayName
+    });
     // first signIn to ga
-    if (creationTime !== lastSignInTime) return;
+    if (user.metadata.creationTime !== user.metadata.lastSignInTime) return;
     sendMetric('Auth', 'newAccountCreation');
   } catch (error) {
     console.error(error);
