8031471ff69b7531751343a146a86fea9dd8d195
Alexander Goryushkin <aleotu@gmail.com>
2024-07-03 21:41:10 -0400

    load aframe-gaussian-splatting-component in index.js instead of dynamic loading

--- src/editor/components/components/AddLayerPanel/createLayerFunctions.js
+++ src/editor/components/components/AddLayerPanel/createLayerFunctions.js
@@ -166,31 +166,18 @@ function createSplatObject() {
     'Please enter a URL to custom Splat model',
     'https://cdn.glitch.me/f80a77a3-62a6-4024-9bef-a6b523d1abc0/gs_Bioswale3_treat.splat'
   );
-  const createSplatElement = () => {
-    if (modelUrl && modelUrl !== '') {
-      const newEl = document.createElement('a-entity');
-      newEl.classList.add('splat-model');
-      newEl.setAttribute('data-no-pause', '');
-      newEl.setAttribute('gaussian_splatting', `src: ${modelUrl}`);
-      newEl.setAttribute('data-layer-name', 'Splat Model • My Custom Object');
-      newEl.play();
-      const parentEl = document.querySelector('#street-container');
-      parentEl.appendChild(newEl);
-      // update sceneGraph
-      Events.emit('entitycreated', newEl);
-    }
-  };
 
-  if (AFRAME.components['gaussian_splatting']) {
-    createSplatElement();
-  } else {
-    loadScript(
-      new URL(
-        '/src/lib/aframe-gaussian-splatting-component.min.js',
-        import.meta.url
-      ),
-      createSplatElement
-    );
+  if (modelUrl && modelUrl !== '') {
+    const newEl = document.createElement('a-entity');
+    newEl.classList.add('splat-model');
+    newEl.setAttribute('data-no-pause', '');
+    newEl.setAttribute('gaussian_splatting', `src: ${modelUrl}`);
+    newEl.setAttribute('data-layer-name', 'Splat Model • My Custom Object');
+    newEl.play();
+    const parentEl = document.querySelector('#street-container');
+    parentEl.appendChild(newEl);
+    // update sceneGraph
+    Events.emit('entitycreated', newEl);
   }
 }
 
