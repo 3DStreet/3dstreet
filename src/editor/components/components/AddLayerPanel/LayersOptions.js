b7bab24a0219828bf49b8782eba46dfe5d6f67d4
Kieran Farr <kieran.farr@gmail.com>
2024-08-17 21:43:45 -0700

    add some emojis for each group

--- src/editor/components/components/AddLayerPanel/LayersOptions.js
+++ src/editor/components/components/AddLayerPanel/LayersOptions.js
@@ -1,48 +1,48 @@
 const LayersOptions = [
   {
     value: 'Street Layers',
-    label: 'Street Layers',
+    label: 'ꈨ Street Layers',
     onClick: () => {}
   },
   {
     value: 'Dividers & Traffic Control',
-    label: 'Dividers & Traffic Control',
+    label: '🚧 Dividers & Traffic Control',
     mixinGroups: ['dividers'],
     onClick: () => {}
   },
   {
     value: 'Motor Vehicles',
-    label: 'Motor Vehicles',
+    label: '🚗 Motor Vehicles',
     mixinGroups: ['vehicles-rigged', 'vehicles-transit'],
     onClick: () => {}
   },
   {
     value: 'People Power Vehicles',
-    label: 'People Power Vehicles',
+    label: '🚲 People Power Vehicles',
     mixinGroups: ['vehicles', 'cyclists'],
     onClick: () => {}
   },
   {
     value: 'Pro Layers',
-    label: 'Pro Layers',
+    label: '🌏 Pro Layers',
     onClick: () => {}
   },
   {
     value: 'Characters',
-    label: 'Characters',
+    label: '🚶 Characters',
     mixinGroups: ['people'],
     onClick: () => {}
   },
   {
     value: 'Sidewalk Props',
-    label: 'Sidewalk Props',
+    label: '🌳 Sidewalk Props',
     mixinGroups: ['sidewalk-props', 'intersection-props'],
     onClick: () => {}
   },
 
   {
-    value: 'Models: Buildings',
-    label: 'Models: Buildings',
+    value: 'Buildings',
+    label: '🏠 Buildings',
     mixinGroups: ['buildings'],
     onClick: () => {}
   }
