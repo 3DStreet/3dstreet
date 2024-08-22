ade1dfb019622e266d00299198893429442c7c6c
Kieran Farr <kieran.farr@gmail.com>
2024-08-22 11:43:37 -0700

    profile image wip

--- src/editor/components/components/ProfileButton/ProfileButton.component.jsx
+++ src/editor/components/components/ProfileButton/ProfileButton.component.jsx
@@ -24,6 +24,53 @@ const ProfileButton = () => {
     return Events.emit('opensigninmodal');
   };
 
+  const lookupMsAzureProfilePhoto = (accessToken) => {
+    fetch('https://graph.microsoft.com/v1.0/me/photo/$value', {
+      // this results in 401 error if a user hasn't set a profile photo
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+        'Content-Type': 'image/jpg'
+      }
+    })
+      .then(async function (response) {
+        return await response.blob();
+      })
+      .then(function (blob) {
+        const imageObjectURL = URL.createObjectURL(blob);
+        // imageObjectURL will be e.g. blob:http://localhost:3000/f123c12a-1234-4e30-4321-af32f2c5e5bc
+        // so updating the <img scr=""> will present the image correctly after
+        // this function runs
+        // setProfilePicUrl(imageObjectURL);
+        return imageObjectURL;
+      })
+      .catch((e) => console.log('error injecting photo'));
+  };
+
+  const getCurrentUserProfilePhotoURL = (currentUser) => {
+    if (!currentUser) {
+      return;
+    }
+    // check if currentUser is not null, then check if photoURL is null
+    console.log(currentUser);
+    if (currentUser.photoURL) {
+      return currentUser.photoURL;
+    } else if (currentUser.providerData[0].providerId === 'microsoft.com') {
+      console.log('microsoft.com', currentUser);
+      console.log(currentUser.accessToken);
+      const imageObjectURLResponse = lookupMsAzureProfilePhoto(
+        currentUser.accessToken
+      );
+      console.log(imageObjectURLResponse);
+      return imageObjectURLResponse;
+      // getRedirectResult(auth)
+      //   .then(result => {
+      //     const accessToken = result.credential.accessToken;
+      //     localStorage.set('accessToken', accessToken);
+      //     console.log(accessToken);
+      //   });
+    }
+  };
+
   return (
     <Button
       className={styles.profileButton}
@@ -33,7 +80,7 @@ const ProfileButton = () => {
       {currentUser ? (
         <img
           className={styles.photoURL}
-          src={currentUser.photoURL}
+          src={getCurrentUserProfilePhotoURL(currentUser)}
           alt="userPhoto"
           referrerPolicy="no-referrer"
         />
