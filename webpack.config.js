204bfe233f5ed771addf319b6106220138e0e628
Rahul Gupta <rahul@MacBook-Air-3.local>
2024-09-17 16:48:44 -0700

    try locally built aframe

--- webpack.config.js
+++ webpack.config.js
@@ -42,6 +42,7 @@ module.exports = {
     new CopyWebpackPlugin({
       patterns: [
         { from: 'src/lib/aframe-mapbox-component.min.js' },
+        { from: 'src/lib/aframe-master.min.js' },
         { from: 'src/notyf.min.css' },
         { from: 'src/viewer-styles.css' }
       ]
