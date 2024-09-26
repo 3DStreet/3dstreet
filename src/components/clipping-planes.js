6198146c9a0f2026f687846c2730205ef10952eb
Rahul Gupta <rahul@MacBook-Air-3.local>
2024-09-26 14:14:57 -0700

    wor

--- src/components/clipping-planes.js
+++ src/components/clipping-planes.js
@@ -29,16 +29,30 @@ AFRAME.registerComponent('clipping-planes', {
   },
 
   updateClipPlanes: function () {
+    var auxQuaternion = new THREE.Quaternion();
+    var auxPosition = new THREE.Vector3();
+    var auxEuler = new THREE.Euler();
+    var auxMatrix = new THREE.Matrix4();
+    var identityQuaternion = new THREE.Quaternion();
+    var auxScale = new THREE.Vector3();
+
+    var parent = this.targetEl.object3D.parent.parent;
     // Update bounding box
-    this.targetBox.setFromObject(this.targetEl.object3D);
+    auxEuler.copy(this.targetEl.object3D.rotation);
+    this.targetEl.object3D.rotation.set(0, 0, 0);
+
+    parent.matrixWorld.decompose(auxPosition, auxQuaternion, auxScale);
+    auxMatrix.compose(auxPosition, identityQuaternion, auxScale);
+    parent.matrixWorld.copy(auxMatrix);
+
+    this.targetBox.setFromObject(this.targetEl.object3D, true);
     this.targetBox.getCenter(this.targetCenter);
     this.targetBox.getSize(this.targetSize);
-
     // Update plane positions
     const normals = [
       new THREE.Vector3(1, 0, 0),
       new THREE.Vector3(-1, 0, 0),
-      new THREE.Vector3(0, 1, 0),
+      new THREE.Vector3(0, 1, 0), // top
       new THREE.Vector3(0, -1, 0),
       new THREE.Vector3(0, 0, 1),
       new THREE.Vector3(0, 0, -1)
@@ -46,20 +60,23 @@ AFRAME.registerComponent('clipping-planes', {
 
     for (let i = 0; i < normals.length; i++) {
       const normal = normals[i];
-      let point = this.targetCenter
-        .clone()
-        .add(
-          normal
-            .clone()
-            .multiply(
-              this.targetSize.clone().multiply(new THREE.Vector3(0.5, 0.5, 0.5))
-            )
-        );
+      let point = this.targetCenter.clone().add(
+        normal
+          .applyQuaternion(auxQuaternion)
+          .clone()
+          .multiply(
+            this.targetSize.clone().multiply(new THREE.Vector3(0.5, 0.5, 0.5))
+          )
+      );
       if (i === 2) {
         // set top clipping plane 100m higher
         point = this.targetCenter.clone().add(new THREE.Vector3(0, 100, 0));
       }
+      // point.applyQuaternion(auxQuaternion)
       this.clipPlanes[i].setFromNormalAndCoplanarPoint(normal, point);
+
+      parent.matrixWorld.compose(auxPosition, auxQuaternion, auxScale);
+      this.targetEl.object3D.rotation.copy(auxEuler);
     }
   },
 
