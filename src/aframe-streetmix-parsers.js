99e0b3778c4d3fd8de4b2aefcee8f4dd4ff9fe1a
Kieran Farr <kieran.farr@gmail.com>
2024-11-22 13:36:51 -0800

    support showVehicles conditional

--- src/aframe-streetmix-parsers.js
+++ src/aframe-streetmix-parsers.js
@@ -899,15 +899,17 @@ function processSegments(
       segmentParentEl.append(tracksParentEl);
     } else if (segments[i].type === 'turn-lane') {
       segmentPreset = 'drive-lane'; // use normal drive lane road material
-      const rotationCloneY = variantList[0] === 'inbound' ? 0 : 180;
-      segmentParentEl.setAttribute(
-        'street-generated-random',
-        `modelsArray: sedan-rig, box-truck-rig, self-driving-waymo-car, suv-rig, motorbike;
-          length: ${length};
-          placeLength: 7.3;
-          facing: ${rotationCloneY};
-          count: ${getRandomIntInclusive(2, 4)};`
-      );
+      if (showVehicles) {
+        const rotationCloneY = variantList[0] === 'inbound' ? 0 : 180;
+        segmentParentEl.setAttribute(
+          'street-generated-random',
+          `modelsArray: sedan-rig, box-truck-rig, self-driving-waymo-car, suv-rig, motorbike;
+            length: ${length};
+            placeLength: 7.3;
+            facing: ${rotationCloneY};
+            count: ${getRandomIntInclusive(2, 4)};`
+        );
+      }
       var markerMixinId = variantList[1]; // set the mixin of the road markings to match the current variant name
       // Fix streetmix inbound turn lane orientation (change left to right) per: https://github.com/streetmix/streetmix/issues/683
       if (variantList[0] === 'inbound') {
@@ -1109,16 +1111,18 @@ function processSegments(
       // add this stencil stuff to the segment parent
       segmentParentEl.append(reusableObjectStencilsParentEl);
     } else if (segments[i].type === 'drive-lane') {
-      // const isAnimated = variantList[2] === 'animated' || globalAnimated;
-      const rotationCloneY = variantList[0] === 'inbound' ? 0 : 180;
-      segmentParentEl.setAttribute(
-        'street-generated-random',
-        `modelsArray: sedan-rig, box-truck-rig, self-driving-waymo-car, suv-rig, motorbike;
-          length: ${length};
-          placeLength: 7.3;
-          facing: ${rotationCloneY};
-          count: ${getRandomIntInclusive(2, 4)};`
-      );
+      if (showVehicles) {
+        // const isAnimated = variantList[2] === 'animated' || globalAnimated;
+        const rotationCloneY = variantList[0] === 'inbound' ? 0 : 180;
+        segmentParentEl.setAttribute(
+          'street-generated-random',
+          `modelsArray: sedan-rig, box-truck-rig, self-driving-waymo-car, suv-rig, motorbike;
+            length: ${length};
+            placeLength: 7.3;
+            facing: ${rotationCloneY};
+            count: ${getRandomIntInclusive(2, 4)};`
+        );
+      }
     } else if (segments[i].type === 'food-truck') {
       segmentPreset = 'drive-lane';
       const rotationCloneY = variantList[0] === 'left' ? 0 : 180;
