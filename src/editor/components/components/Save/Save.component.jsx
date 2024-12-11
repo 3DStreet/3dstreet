9fa71ce1fc4f1cc4354dee20aa3d3299539b2680
Vincent Fretin <vincent.fretin@gmail.com>
2024-12-11 15:48:39 +0100

    less moving parts during the saved animation

--- src/editor/components/components/Save/Save.component.jsx
+++ src/editor/components/components/Save/Save.component.jsx
@@ -89,19 +89,23 @@ export const Save = ({ currentUser }) => {
   return (
     <div>
       {currentUser ? (
-        <div className="saveButtonWrapper relative w-24">
+        <div className="saveButtonWrapper relative">
           {isSavingScene ? (
-            <Button variant="filled">
-              <div>Saved</div>
+            <Button
+              leadingIcon={<Save24Icon />}
+              variant="filled"
+              className="min-w-[110px]"
+            >
+              <div className="grow">Saved</div>
             </Button>
           ) : (
             <Button
               leadingIcon={<Save24Icon />}
               onClick={toggleSaveActionState}
-              disabled={isSavingScene}
               variant="toolbtn"
+              className="min-w-[110px]"
             >
-              <div>Save</div>
+              <div className="grow">Save</div>
             </Button>
           )}
           {isSaveActionActive && (
@@ -128,11 +132,11 @@ export const Save = ({ currentUser }) => {
       ) : (
         <Button
           leadingIcon={<Save24Icon />}
-          onClick={handleUnsignedSave}
-          disabled={isSavingScene}
+          onClick={!isSavingScene ? handleUnsignedSave : undefined}
           variant="toolbtn"
+          className="min-w-[110px]"
         >
-          <div>Save</div>
+          <div className="grow">Save</div>
         </Button>
       )}
     </div>
