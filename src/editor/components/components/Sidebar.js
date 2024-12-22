24208189298ae3b9c8fd24d1f661feb0d0f48105
Kieran Farr <kieran.farr@gmail.com>
2024-12-21 19:44:37 -0800

    barely working theatre

--- src/editor/components/components/Sidebar.js
+++ src/editor/components/components/Sidebar.js
@@ -8,7 +8,7 @@ import ComponentsContainer from './ComponentsContainer';
 import Events from '../../lib/Events';
 import Mixins from './Mixins';
 import PropTypes from 'prop-types';
-import React from 'react';
+import { useEffect, useState } from 'react';
 import capitalize from 'lodash-es/capitalize';
 import classnames from 'classnames';
 import {
@@ -17,181 +17,185 @@ import {
   SegmentIcon,
   ManagedStreetIcon
 } from '../../icons';
-import GeoSidebar from './GeoSidebar'; // Make sure to create and import this new component
+import GeoSidebar from './GeoSidebar';
 import IntersectionSidebar from './IntersectionSidebar';
 import StreetSegmentSidebar from './StreetSegmentSidebar';
 import ManagedStreetSidebar from './ManagedStreetSidebar';
 import AdvancedComponents from './AdvancedComponents';
-export default class Sidebar extends React.Component {
-  static propTypes = {
-    entity: PropTypes.object,
-    visible: PropTypes.bool
-  };
+import { useTheatre } from '../../contexts/TheatreContext';
 
-  constructor(props) {
-    super(props);
-    this.state = {
-      showSideBar: true
+export default function Sidebar({ entity, visible }) {
+  const [showSideBar, setShowSideBar] = useState(true);
+  const { addEntityToTheatre, controlledEntities } = useTheatre();
+
+  useEffect(() => {
+    const onEntityUpdate = (detail) => {
+      if (detail.entity !== entity) {
+        return;
+      }
+      if (
+        detail.component === 'mixin' ||
+        detail.component === 'data-layer-name'
+      ) {
+        // Force update happens automatically in functional components
+      }
     };
+
+    const onComponentRemove = (detail) => {
+      if (detail.entity !== entity) {
+        return;
+      }
+      // Force update happens automatically
+    };
+
+    const onComponentAdd = (detail) => {
+      if (detail.entity !== entity) {
+        return;
+      }
+      // Force update happens automatically
+    };
+
+    Events.on('entityupdate', onEntityUpdate);
+    Events.on('componentremove', onComponentRemove);
+    Events.on('componentadd', onComponentAdd);
+
+    return () => {
+      Events.off('entityupdate', onEntityUpdate);
+      Events.off('componentremove', onComponentRemove);
+      Events.off('componentadd', onComponentAdd);
+    };
+  }, [entity]);
+
+  const toggleRightBar = () => {
+    setShowSideBar(!showSideBar);
+  };
+
+  if (!entity || !visible) {
+    return <div />;
   }
 
-  onEntityUpdate = (detail) => {
-    if (detail.entity !== this.props.entity) {
-      return;
-    }
-    if (
-      detail.component === 'mixin' ||
-      detail.component === 'data-layer-name'
-    ) {
-      this.forceUpdate();
-    }
-  };
+  const entityName = entity.getDOMAttribute('data-layer-name');
+  const entityMixin = entity.getDOMAttribute('mixin');
+  const formattedMixin = entityMixin
+    ? capitalize(entityMixin.replaceAll('-', ' ').replaceAll('_', ' '))
+    : null;
 
-  onComponentRemove = (detail) => {
-    if (detail.entity !== this.props.entity) {
-      return;
-    }
-    this.forceUpdate();
-  };
+  const className = classnames({
+    outliner: true,
+    hide: showSideBar,
+    'mt-16': true
+  });
 
-  onComponentAdd = (detail) => {
-    if (detail.entity !== this.props.entity) {
-      return;
-    }
-    this.forceUpdate();
-  };
-
-  componentDidMount() {
-    Events.on('entityupdate', this.onEntityUpdate);
-    Events.on('componentremove', this.onComponentRemove);
-    Events.on('componentadd', this.onComponentAdd);
-  }
-
-  componentWillUnmount() {
-    Events.off('entityupdate', this.onEntityUpdate);
-    Events.off('componentremove', this.onComponentRemove);
-    Events.off('componentadd', this.onComponentAdd);
-  }
-
-  // additional toggle for hide/show panel by clicking the button
-  toggleRightBar = () => {
-    this.setState({ showSideBar: !this.state.showSideBar });
-  };
-
-  render() {
-    const entity = this.props.entity;
-    const visible = this.props.visible;
-    const className = classnames({
-      outliner: true,
-      hide: this.state.showSideBar,
-      'mt-16': true
-    });
-    if (entity && visible) {
-      const entityName = entity.getDOMAttribute('data-layer-name');
-      const entityMixin = entity.getDOMAttribute('mixin');
-      const formattedMixin = entityMixin
-        ? capitalize(entityMixin.replaceAll('-', ' ').replaceAll('_', ' '))
-        : null;
-      return (
-        <div className={className} tabIndex="0">
-          {this.state.showSideBar ? (
-            <>
-              <div id="layers-title" onClick={this.toggleRightBar}>
-                <div className={'layersBlock'}>
-                  {entity.getAttribute('managed-street') ? (
-                    <ManagedStreetIcon />
-                  ) : entity.getAttribute('street-segment') ? (
-                    <SegmentIcon />
-                  ) : (
-                    <Object24Icon />
-                  )}
-                  <span>{entityName || formattedMixin}</span>
-                </div>
-                <div id="toggle-rightbar">
-                  <ArrowRightIcon />
-                </div>
-              </div>
-              <div className="scroll">
-                {entity.id !== 'reference-layers' &&
-                !entity.getAttribute('street-segment') ? (
-                  <>
-                    {!!entity.mixinEls.length && <Mixins entity={entity} />}
-                    {entity.hasAttribute('data-no-transform') ? (
-                      <></>
-                    ) : (
-                      <div id="sidebar-buttons">
-                        <Button
-                          variant={'toolbtn'}
-                          onClick={() => renameEntity(entity)}
-                        >
-                          Rename
-                        </Button>
-                        <Button
-                          variant={'toolbtn'}
-                          onClick={() => cloneEntity(entity)}
-                        >
-                          Duplicate
-                        </Button>
-                        <Button
-                          variant={'toolbtn'}
-                          onClick={() => removeSelectedEntity()}
-                        >
-                          Delete
-                        </Button>
-                      </div>
-                    )}
-                    {entity.getAttribute('intersection') && (
-                      <IntersectionSidebar entity={entity} />
-                    )}
-                    {entity.getAttribute('managed-street') && (
-                      <ManagedStreetSidebar entity={entity} />
-                    )}
-                    <ComponentsContainer entity={entity} />
-                  </>
+  return (
+    <div className={className} tabIndex="0">
+      {showSideBar ? (
+        <>
+          <div id="layers-title" onClick={toggleRightBar}>
+            <div className={'layersBlock'}>
+              {entity.getAttribute('managed-street') ? (
+                <ManagedStreetIcon />
+              ) : entity.getAttribute('street-segment') ? (
+                <SegmentIcon />
+              ) : (
+                <Object24Icon />
+              )}
+              <span>{entityName || formattedMixin}</span>
+            </div>
+            <div id="toggle-rightbar">
+              <ArrowRightIcon />
+            </div>
+          </div>
+          <div className="scroll">
+            {entity.id !== 'reference-layers' &&
+            !entity.getAttribute('street-segment') ? (
+              <>
+                {!!entity.mixinEls.length && <Mixins entity={entity} />}
+                {entity.hasAttribute('data-no-transform') ? (
+                  <></>
                 ) : (
+                  <div id="sidebar-buttons">
+                    <Button
+                      variant={'toolbtn'}
+                      onClick={() => renameEntity(entity)}
+                    >
+                      Rename
+                    </Button>
+                    <Button
+                      variant={'toolbtn'}
+                      onClick={() => cloneEntity(entity)}
+                    >
+                      Duplicate
+                    </Button>
+                    <Button
+                      variant={'toolbtn'}
+                      onClick={() => removeSelectedEntity()}
+                    >
+                      Delete
+                    </Button>
+                    <Button
+                      variant={'toolbtn'}
+                      onClick={() => addEntityToTheatre(entity)}
+                      disabled={controlledEntities.has(entity.id)}
+                    >
+                      {controlledEntities.has(entity.id)
+                        ? 'Added to Animation'
+                        : 'Add to Animation'}
+                    </Button>
+                  </div>
+                )}
+                {entity.getAttribute('intersection') && (
+                  <IntersectionSidebar entity={entity} />
+                )}
+                {entity.getAttribute('managed-street') && (
+                  <ManagedStreetSidebar entity={entity} />
+                )}
+                <ComponentsContainer entity={entity} />
+              </>
+            ) : (
+              <>
+                {entity.getAttribute('street-segment') && (
                   <>
-                    {entity.getAttribute('street-segment') && (
-                      <>
-                        <StreetSegmentSidebar entity={entity} />
-                        <div className="advancedComponentsContainer">
-                          <AdvancedComponents entity={entity} />
-                        </div>
-                      </>
-                    )}
-                    {entity.id === 'reference-layers' && (
-                      <GeoSidebar entity={entity} />
-                    )}
+                    <StreetSegmentSidebar entity={entity} />
+                    <div className="advancedComponentsContainer">
+                      <AdvancedComponents entity={entity} />
+                    </div>
                   </>
                 )}
+                {entity.id === 'reference-layers' && (
+                  <GeoSidebar entity={entity} />
+                )}
+              </>
+            )}
+          </div>
+        </>
+      ) : (
+        <>
+          <div
+            onClick={toggleRightBar}
+            className="relative flex items-center justify-end"
+          >
+            <div className="group relative flex cursor-pointer items-center p-2">
+              <span className="absolute right-12 whitespace-nowrap rounded bg-gray-800 px-2 py-1 text-white opacity-0 transition-all duration-300 group-hover:opacity-100">
+                {entityName || formattedMixin}
+              </span>
+              <div className="relative z-10">
+                {entity.getAttribute('managed-street') ? (
+                  <ManagedStreetIcon />
+                ) : entity.getAttribute('street-segment') ? (
+                  <SegmentIcon />
+                ) : (
+                  <Object24Icon />
+                )}
               </div>
-            </>
-          ) : (
-            <>
-              <div
-                onClick={this.toggleRightBar}
-                className="relative flex items-center justify-end"
-              >
-                <div className="group relative flex cursor-pointer items-center p-2">
-                  <span className="absolute right-12 whitespace-nowrap rounded bg-gray-800 px-2 py-1 text-white opacity-0 transition-all duration-300 group-hover:opacity-100">
-                    {entityName || formattedMixin}
-                  </span>
-                  <div className="relative z-10">
-                    {entity.getAttribute('managed-street') ? (
-                      <ManagedStreetIcon />
-                    ) : entity.getAttribute('street-segment') ? (
-                      <SegmentIcon />
-                    ) : (
-                      <Object24Icon />
-                    )}
-                  </div>
-                </div>
-              </div>
-            </>
-          )}
-        </div>
-      );
-    } else {
-      return <div />;
-    }
-  }
+            </div>
+          </div>
+        </>
+      )}
+    </div>
+  );
 }
+
+Sidebar.propTypes = {
+  entity: PropTypes.object,
+  visible: PropTypes.bool
+};
--- src/editor/contexts/TheatreContext.js
+++ src/editor/contexts/TheatreContext.js
@@ -1,26 +1,155 @@
-import { createContext, useEffect } from 'react';
+import { createContext, useContext, useEffect, useState } from 'react';
 import { getProject } from '@theatre/core';
 import studio from '@theatre/studio';
 
 export const TheatreContext = createContext();
 
+export function useTheatre() {
+  return useContext(TheatreContext);
+}
+
 export function TheatreProvider({ children }) {
+  const [project, setProject] = useState(null);
+  const [sheet, setSheet] = useState(null);
+  const [controlledEntities, setControlledEntities] = useState(new Set());
+
   useEffect(() => {
     // Initialize Theatre.js
     studio.initialize();
 
     // Create a project
-    const project = getProject('3DStreet Animation');
-    const sheet = project.sheet('Main Sheet');
-    console.log('[theatre] project', project);
-    console.log('[theatre] sheet', sheet);
+    const proj = getProject('3DStreet Animation');
+    const mainSheet = proj.sheet('Main Sheet');
+
+    setProject(proj);
+    setSheet(mainSheet);
+
+    console.log('[theatre] project', proj);
+    console.log('[theatre] sheet', mainSheet);
 
     return () => {
       // Cleanup if needed
     };
   }, []);
 
+  const createValidObjectId = (entity) => {
+    // Get entity name or mixin as base
+    const baseName =
+      entity.getDOMAttribute('data-layer-name') ||
+      entity.getDOMAttribute('mixin') ||
+      'unnamed';
+
+    // Clean up the name to be valid for Theatre.js
+    const cleanName = baseName
+      .replace(/[^a-zA-Z0-9]/g, '_') // Replace non-alphanumeric with underscore
+      .replace(/^[^a-zA-Z]/, 'obj_$&') // Ensure starts with letter
+      .replace(/_{2,}/g, '_'); // Remove duplicate underscores
+
+    // Add a unique suffix using timestamp
+    const uniqueName = `${cleanName}_${Date.now()}`;
+
+    console.log(
+      '[theatre] Created object ID:',
+      uniqueName,
+      'for entity:',
+      entity
+    );
+    return uniqueName;
+  };
+
+  const addEntityToTheatre = (entity) => {
+    if (!sheet || !entity) {
+      console.warn('[theatre] Cannot add entity - sheet or entity missing', {
+        sheet,
+        entity
+      });
+      return;
+    }
+
+    if (controlledEntities.has(entity.id)) {
+      console.log('[theatre] Entity already controlled:', entity.id);
+      return;
+    }
+
+    console.log('[theatre] Adding entity to control:', entity);
+
+    // Generate valid object ID
+    const objectId = createValidObjectId(entity);
+
+    try {
+      // Get current transform values
+      const position = entity.object3D.position;
+      const rotation = entity.object3D.rotation;
+      const material = entity.getAttribute('material');
+
+      // Create a new object for the entity
+      const entityObj = sheet.object(objectId, {
+        position: {
+          x: position.x,
+          y: position.y,
+          z: position.z
+        },
+        rotation: {
+          x: rotation.x,
+          y: rotation.y,
+          z: rotation.z
+        },
+        ...(material ? { opacity: material.opacity || 1 } : {})
+      });
+
+      console.log('[theatre] Created object:', objectId, entityObj);
+
+      // Subscribe to changes
+      entityObj.onValuesChange((values) => {
+        const { position, rotation, opacity } = values;
+
+        // Update position
+        if (position) {
+          entity.object3D.position.set(position.x, position.y, position.z);
+        }
+
+        // Update rotation
+        if (rotation) {
+          entity.object3D.rotation.set(rotation.x, rotation.y, rotation.z);
+        }
+
+        // Update opacity if material exists
+        if (opacity !== undefined && entity.getAttribute('material')) {
+          entity.setAttribute('material', 'opacity', opacity);
+        }
+      });
+
+      // Add to controlled entities set
+      setControlledEntities((prev) => new Set(prev).add(entity.id));
+
+      console.log('[theatre] Successfully added entity to control');
+    } catch (error) {
+      console.error('[theatre] Error adding entity to control:', error);
+    }
+  };
+
+  const removeEntityFromTheatre = (entityId) => {
+    if (!sheet || !controlledEntities.has(entityId)) return;
+
+    // Remove object from sheet
+    setControlledEntities((prev) => {
+      const next = new Set(prev);
+      next.delete(entityId);
+      return next;
+    });
+  };
+
   return (
-    <TheatreContext.Provider value={{}}>{children}</TheatreContext.Provider>
+    <TheatreContext.Provider
+      value={{
+        project,
+        sheet,
+        controlledEntities,
+        addEntityToTheatre,
+        removeEntityFromTheatre
+      }}
+    >
+      {children}
+    </TheatreContext.Provider>
   );
 }
