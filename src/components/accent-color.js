8176239880856b2c94421450551b10fcaf1b1cb9
Edward Wei <nyw6dh@virginia.edu>
2024-12-29 21:23:14 -0800

    Use model loaded hook and change mesh traverse

--- src/components/accent-color.js
+++ src/components/accent-color.js
@@ -10,24 +10,23 @@ AFRAME.registerComponent('accent-color', {
     }
   },
   init: function () {
-    this.meshes = [];
-    // The group children aren't ready when the component is initialized, so wait a bit.
-    // TODO: Find a better way to time this.
-    setTimeout(() => {
-      this.el.object3D.traverse((o) => o.material && this.meshes.push(o));
-      this.update();
-    }, 1000);
+    // Want the color update to apply when the mixin is loaded,
+    // which may be at a different time than the component
+    this.el.addEventListener('model-loaded', () => this.update());
   },
   update: function (oldData) {
+    // Collect meshes
+    const meshes = [];
+    this.el.object3D.traverse((o) => o.material && meshes.push(o));
     // Remove the tint on the previous mesh
     if (oldData && oldData.index !== this.data.index) {
-      const oldMesh = this.meshes.at(oldData.index);
+      const oldMesh = meshes.at(oldData.index);
       oldMesh && oldMesh.material.color.set(1, 1, 1);
     }
     // Ignore negative index
     if (this.data.index < 0) return;
     // Apply tint to selected mesh
-    const mesh = this.meshes.at(this.data.index);
+    const mesh = meshes.at(this.data.index);
     if (mesh) {
       mesh.material.color.set(this.data.color);
       mesh.material.color.multiplyScalar(255);
