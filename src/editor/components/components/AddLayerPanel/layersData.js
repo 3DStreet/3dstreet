76a0c0f56c5ea1397f1da3745cf893aed221e2dd
Kieran Farr <kieran.farr@gmail.com>
2025-01-01 17:34:52 -0800

    fix street card ordering

--- src/editor/components/components/AddLayerPanel/layersData.js
+++ src/editor/components/components/AddLayerPanel/layersData.js
@@ -2,12 +2,21 @@ import * as createFunctions from './createLayerFunctions';
 
 export const streetLayersData = [
   {
+    name: 'Create Intersection',
+    img: '',
+    requiresPro: true,
+    icon: 'ui_assets/cards/icons/3dst24.png',
+    description: 'Create 90º intersection entity.',
+    id: 1,
+    handlerFunction: createFunctions.createIntersection
+  },
+  {
     name: 'Street from Streetmix URL',
     img: 'ui_assets/cards/streetmix.jpg',
     icon: 'ui_assets/cards/icons/streetmix24.png',
     description:
       'Create an additional Streetmix street in your 3DStreet scene without replacing any existing streets.',
-    id: 1,
+    id: 2,
     handlerFunction: createFunctions.createStreetmixStreet
   },
   {
@@ -15,7 +24,7 @@ export const streetLayersData = [
     img: 'ui_assets/cards/street-preset-40-24.jpg',
     icon: 'ui_assets/cards/icons/streetmix24.png',
     description: 'Premade Street 40ft Right of Way / 24ft Roadway Width',
-    id: 2,
+    id: 3,
     handlerFunction: createFunctions.create40ftRightOfWay
   },
   {
@@ -23,7 +32,7 @@ export const streetLayersData = [
     img: 'ui_assets/cards/street-preset-60-36.jpg',
     icon: 'ui_assets/cards/icons/streetmix24.png',
     description: 'Premade Street 60ft Right of Way / 36ft Roadway Width',
-    id: 3,
+    id: 4,
     handlerFunction: createFunctions.create60ftRightOfWay
   },
   {
@@ -31,7 +40,7 @@ export const streetLayersData = [
     img: 'ui_assets/cards/street-preset-80-56.jpg',
     icon: 'ui_assets/cards/icons/streetmix24.png',
     description: 'Premade Street 80ft Right of Way / 56ft Roadway Width',
-    id: 4,
+    id: 5,
     handlerFunction: createFunctions.create80ftRightOfWay
   },
   {
@@ -39,7 +48,7 @@ export const streetLayersData = [
     img: 'ui_assets/cards/street-preset-94-70.jpg',
     icon: 'ui_assets/cards/icons/streetmix24.png',
     description: 'Premade Street 94ft Right of Way / 70ft Roadway Width',
-    id: 5,
+    id: 6,
     handlerFunction: createFunctions.create94ftRightOfWay
   },
   {
@@ -47,44 +56,34 @@ export const streetLayersData = [
     img: 'ui_assets/cards/street-preset-150-124.jpg',
     icon: 'ui_assets/cards/icons/streetmix24.png',
     description: 'Premade Street 150ft Right of Way / 124ft Roadway Width',
-    id: 6,
-    handlerFunction: createFunctions.create150ftRightOfWay
-  },
-  {
-    name: 'Create intersection',
-    img: '',
-    requiresPro: true,
-    icon: '',
-    description:
-      'Create intersection entity. Parameters of intersection component could be changed in properties panel.',
     id: 7,
-    handlerFunction: createFunctions.createIntersection
+    handlerFunction: createFunctions.create150ftRightOfWay
   },
   {
     name: '(Beta) Managed Street from Streetmix URL',
     img: '',
     requiresPro: true,
-    icon: '',
+    icon: 'ui_assets/cards/icons/streetmix24.png',
     description:
       'Create a new street from Streetmix URL using the Managed Street component.',
     id: 8,
     handlerFunction: createFunctions.createManagedStreetFromStreetmixURLPrompt
   },
   {
-    name: '(Beta) Managed Street 60ft RoW / 36ft Roadway Width',
-    img: 'ui_assets/cards/street-preset-60-36.jpg',
-    icon: 'ui_assets/cards/icons/3dst24.png',
-    description: 'Premade Street 60ft Right of Way / 36ft Roadway Width',
-    id: 9,
-    handlerFunction: createFunctions.create60ftRightOfWayManagedStreet
-  },
-  {
     name: '(Beta) Managed Street 40ft RoW / 24ft Roadway Width',
     img: 'ui_assets/cards/street-preset-40-24.jpg',
     icon: 'ui_assets/cards/icons/3dst24.png',
     description: 'Premade Street 40ft Right of Way / 24ft Roadway Width',
-    id: 10,
+    id: 9,
     handlerFunction: createFunctions.create40ftRightOfWayManagedStreet
+  },
+  {
+    name: '(Beta) Managed Street 60ft RoW / 36ft Roadway Width',
+    img: 'ui_assets/cards/street-preset-60-36.jpg',
+    icon: 'ui_assets/cards/icons/3dst24.png',
+    description: 'Premade Street 60ft Right of Way / 36ft Roadway Width',
+    id: 10,
+    handlerFunction: createFunctions.create60ftRightOfWayManagedStreet
   },
   {
     name: '(Beta) Managed Street 80ft RoW / 56ft Roadway Width',
