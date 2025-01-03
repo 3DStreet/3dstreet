b28fe73742abfda6f369b6d8a2f2d70a46579b5b
Edward Wei <nyw6dh@virginia.edu>
2025-01-02 20:27:33 -0800

    Fix entity updating when model is changed

--- src/components/custom-colors.js
+++ src/components/custom-colors.js
@@ -11,8 +11,15 @@ AFRAME.registerComponent('custom-colors', {
   update() {
     // Save the original color values if not done already
     if (this.origMaterialMap.size === 0) {
+      const materialMap = new Map();
       this.el.object3D.traverse((node) => {
         if (node.material) {
+          // Duplicate the materials to avoid sharing references across entities
+          if (!materialMap.has(node.material.uuid)) {
+            materialMap.set(node.material.uuid, node.material.clone());
+          }
+          node.material = materialMap.get(node.material.uuid);
+
           this.origMaterialMap.set(
             node.material.uuid,
             node.material.color.clone()
@@ -22,7 +29,6 @@ AFRAME.registerComponent('custom-colors', {
     }
 
     const materials = getMaterials(this.el.object3D);
-    console.log(this.el, materials);
     materials.forEach((material) => {
       if (this.data[material.name] !== undefined) {
         material.color.set(this.data[material.name]);
@@ -35,18 +41,29 @@ AFRAME.registerComponent('custom-colors', {
   updateMaterials() {
     this.update();
   },
+  resetAndUpdateMaterials() {
+    this.origMaterialMap.clear();
+    this.updateMaterials();
+  },
   init() {
     this.origMaterialMap = new Map();
-    this.updateMaterials = this.updateMaterials.bind(this);
+    this.resetAndUpdateMaterials = this.resetAndUpdateMaterials.bind(this);
+
+    // Models that are components of larger models trigger this event instead of model-loaded.
+    // This also will fire when the selected model is changed.
+    this.el.addEventListener('object3dset', this.resetAndUpdateMaterials);
+
     if (this.el.getObject3D('mesh')) {
       this.update();
     } else {
-      this.el.addEventListener('model-loaded', this.updateMaterials);
+      this.updateMaterials = this.updateMaterials.bind(this);
+      this.el.addEventListener('model-loaded', this.updateMaterials, {
+        once: true
+      });
     }
   },
   remove() {
-    console.log('component removed');
-    this.el.removeEventListener('model-loaded', this.updateMaterials);
+    this.el.removeEventListener('object3dset', this.resetAndUpdateMaterials);
     const materials = getMaterials(this.el.object3D);
     materials.forEach((material) => {
       // Reset to default, no tint
