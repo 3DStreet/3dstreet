8f9b8459927276c7bdec436849fb1f91ca7a4cbd
Kieran Farr <kieran.farr@gmail.com>
2025-01-06 15:24:26 -0800

    use three.js traverse

--- src/components/polygon-offset.js
+++ src/components/polygon-offset.js
@@ -1,53 +1,39 @@
 // Component to fix z-fighting by adding polygon offset to meshes
 AFRAME.registerComponent('polygon-offset', {
   schema: {
-    // Negative values move fragments closer to camera; factor and units are multiplied together
+    // Negative values move fragments closer to camera
     factor: { type: 'number', default: -2 },
     units: { type: 'number', default: -2 }
   },
 
   init: function () {
-    // Bind the update method to maintain correct context
-    this.updateMesh = this.updateMesh.bind(this);
-
     // Initial update when mesh is loaded
     const mesh = this.el.getObject3D('mesh');
     if (mesh) {
-      this.updateMesh(mesh);
+      this.applyPolygonOffsetToObject(mesh);
     }
 
     // Listen for model-loaded event
     this.el.addEventListener('model-loaded', (evt) => {
       const mesh = this.el.getObject3D('mesh');
-      this.updateMesh(mesh);
+      this.applyPolygonOffsetToObject(mesh);
     });
   },
 
-  updateMesh: function (mesh) {
-    if (!mesh) return;
+  applyPolygonOffsetToObject: function (object3D) {
+    if (!object3D) return;
 
-    // Function to recursively process materials
-    const processMaterials = (object) => {
-      if (object.material) {
-        // Handle single material
-        if (!Array.isArray(object.material)) {
-          this.updateMaterial(object.material);
-        } else {
-          // Handle multiple materials
-          object.material.forEach((material) => {
+    object3D.traverse((obj) => {
+      if (obj.type === 'Mesh') {
+        if (Array.isArray(obj.material)) {
+          obj.material.forEach((material) => {
             this.updateMaterial(material);
           });
+        } else {
+          this.updateMaterial(obj.material);
         }
       }
-      if (object.children) {
-        object.children.forEach((child) => {
-          processMaterials(child);
-        });
-      }
-    };
-
-    // Start processing from the root mesh
-    processMaterials(mesh);
+    });
   },
 
   updateMaterial: function (material) {
@@ -65,7 +51,30 @@ AFRAME.registerComponent('polygon-offset', {
     // Handle property updates
     const mesh = this.el.getObject3D('mesh');
     if (mesh) {
-      this.updateMesh(mesh);
+      this.applyPolygonOffsetToObject(mesh);
+    }
+  },
+
+  remove: function () {
+    const mesh = this.el.getObject3D('mesh');
+    if (mesh) {
+      mesh.traverse((obj) => {
+        if (obj.type === 'Mesh') {
+          if (Array.isArray(obj.material)) {
+            obj.material.forEach((material) => {
+              material.polygonOffset = false;
+              material.polygonOffsetFactor = 0;
+              material.polygonOffsetUnits = 0;
+              material.needsUpdate = true;
+            });
+          } else if (obj.material) {
+            obj.material.polygonOffset = false;
+            obj.material.polygonOffsetFactor = 0;
+            obj.material.polygonOffsetUnits = 0;
+            obj.material.needsUpdate = true;
+          }
+        }
+      });
     }
   }
 });
