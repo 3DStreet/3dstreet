e2b9a845a331335574d9bb22d2491e1cebc285dd
Kieran Farr <kieran.farr@gmail.com>
2025-02-20 11:33:25 -0800

    depthTest false for rulerCursorEntity

--- src/editor/components/components/ActionBar/ActionBar.component.jsx
+++ src/editor/components/components/ActionBar/ActionBar.component.jsx
@@ -37,10 +37,10 @@ const ActionBar = ({ selectedEntity }) => {
       rulerCursorEntity.setAttribute('id', 'rulerCursorEntity');
       rulerCursorEntity.classList.add('hideFromSceneGraph');
       rulerCursorEntity.innerHTML = `
-          <a-ring class="hideFromSceneGraph" rotation="-90 0 0" radius-inner="0.2" radius-outer="0.3">
-            <a-ring class="hideFromSceneGraph" color="yellow" radius-inner="0.4" radius-outer="0.5"
+          <a-ring class="hideFromSceneGraph" rotation="-90 0 0" material="depthTest: false" radius-inner="0.2" radius-outer="0.3">
+            <a-ring class="hideFromSceneGraph" color="yellow" material="depthTest: false" radius-inner="0.4" radius-outer="0.5"
               animation="property: scale; from: 1 1 1; to: 2 2 2; loop: true; dir: alternate"></a-ring>
-            <a-ring class="hideFromSceneGraph" color="yellow" radius-inner="0.6" radius-outer="0.7"
+            <a-ring class="hideFromSceneGraph" color="yellow" material="depthTest: false" radius-inner="0.6" radius-outer="0.7"
               animation="property: scale; from: 1 1 1; to: 3 3 3; loop: true; dir: alternate"></a-ring>
             <a-entity class="hideFromSceneGraph" rotation="90 0 0">
               <a-cylinder class="hideFromSceneGraph" color="yellow" position="0 5.25 0" radius="0.05" height="2.5"></a-cylinder>
