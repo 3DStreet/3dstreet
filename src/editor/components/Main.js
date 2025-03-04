5076a8fe96d8fa3a48545808af659a0b4e37b8db
Kieran Farr <kieran.farr@gmail.com>
2025-03-04 13:25:28 -0800

    fix toggle on/off of sidebar

--- src/editor/components/Main.js
+++ src/editor/components/Main.js
@@ -55,24 +55,19 @@ export default function Main() {
       }));
     });
     Events.on('togglesidebar', (event) => {
+      console.log('event.which', event.which);
       if (event.which === 'all') {
-        if (state.visible.scenegraph || state.visible.attributes) {
-          setState((prevState) => ({
+        setState((prevState) => {
+          const isVisible =
+            prevState.visible.scenegraph || prevState.visible.attributes;
+          return {
             ...prevState,
             visible: {
-              scenegraph: false,
-              attributes: false
+              scenegraph: !isVisible,
+              attributes: !isVisible
             }
-          }));
-        } else {
-          setState((prevState) => ({
-            ...prevState,
-            visible: {
-              scenegraph: true,
-              attributes: true
-            }
-          }));
-        }
+          };
+        });
       } else if (event.which === 'attributes') {
         setState((prevState) => ({
           visible: {
