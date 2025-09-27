bc1c4dae3b2366c4fb8e091181da11a24517e8a7
Kieran Farr <kieran.farr@gmail.com>
2025-09-27 09:52:23 -0700

    add sentry tracking

--- public/functions/webxr-variant.js
+++ public/functions/webxr-variant.js
@@ -13,7 +13,8 @@ exports.serveWebXRVariant = functions.https.onRequest((req, res) => {
   <script src="https://aframe.io/releases/1.7.1/aframe.min.js"></script>
   <!-- 3dstreet -->
   <script src="/dist/aframe-street-component.js"></script>
-
+  <!-- sentry -->
+  <script src="https://js.sentry-cdn.com/941ec1dfd6b01567fe3fd16db9cbaa54.min.js" crossorigin="anonymous"></script>
   <!-- mapbox -->
   <script src="/dist/aframe-mapbox-component.min.js"></script>
 
