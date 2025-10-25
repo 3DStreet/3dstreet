ef0c102ff5558ab1c0f0ea604ff1414aef9e2147
Kieran Farr <kieran.farr@gmail.com>
2025-10-25 14:40:25 -0700

    kontext pro should be default

--- src/image-playground/generator.js
+++ src/image-playground/generator.js
@@ -183,7 +183,7 @@ const GeneratorTab = {
                             <option value="flux-pro">Flux Pro</option>
                             <option value="flux-dev">Flux Dev</option>
                             <option value="flux-pro-1.1-ultra">Flux Ultra</option>
-                            <option value="flux-kontext-pro">Flux Kontext Pro</option>
+                            <option value="flux-kontext-pro" selected>Flux Kontext Pro</option>
                             <option value="flux-kontext-max">Flux Kontext Max</option>
                         </select>
                     </div>
