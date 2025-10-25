bdbee5c6d8817c89988128525ae5cd94e0fb7357
Kieran Farr <kieran.farr@gmail.com>
2025-10-25 15:50:03 -0700

    update gallery toggle button
    put at top and enlarge
    have arrows point in the direction of change when button is clicked

--- src/image-generator/gallery.js
+++ src/image-generator/gallery.js
@@ -389,8 +389,8 @@ const FluxGallery = {
       const isCollapsed =
         galleryContainer.classList.contains('gallery-collapsed');
       galleryToggle.innerHTML = isCollapsed
-        ? '<svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 5l7 7-7 7M5 5l7 7-7 7" /></svg>'
-        : '<svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M11 19l-7-7 7-7m8 14l-7-7 7-7" /></svg>';
+        ? '<svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M11 19l-7-7 7-7m8 14l-7-7 7-7" /></svg>'
+        : '<svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 5l7 7-7 7M5 5l7 7-7 7" /></svg>';
     });
 
     // Pagination events
