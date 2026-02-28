08ee06cfba5855395d8720e14658cfc3ea9036cf
Vincent Fretin <vincent.fretin@gmail.com>
2026-02-28 16:20:17 +0100

    reformat file

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,15 +1,15 @@
-import neostandard from "neostandard";
-import reactPlugin from "eslint-plugin-react";
-import reactHooks from "eslint-plugin-react-hooks";
+import neostandard from 'neostandard';
+import reactPlugin from 'eslint-plugin-react';
+import reactHooks from 'eslint-plugin-react-hooks';
 
 export default [
   // Global ignores
   {
-    ignores: ["src/lib", "public", "dist", ".storybook"],
+    ignores: ['src/lib', 'public', 'dist', '.storybook']
   },
 
   // neostandard base config, no style rules, browser globals
-  ...neostandard({ noStyle: true, env: ["browser"] }),
+  ...neostandard({ noStyle: true, env: ['browser'] }),
 
   // React recommended rules
   reactPlugin.configs.flat.recommended,
@@ -20,33 +20,33 @@ export default [
 
   // Project config
   {
-    files: ["src/**/*.{js,jsx}"],
+    files: ['src/**/*.{js,jsx}'],
     languageOptions: {
       parserOptions: {
-        ecmaFeatures: { jsx: true },
+        ecmaFeatures: { jsx: true }
       },
       globals: {
-        STREET: "readonly",
-        AFRAME: "readonly",
-        THREE: "readonly",
-      },
+        STREET: 'readonly',
+        AFRAME: 'readonly',
+        THREE: 'readonly'
+      }
     },
     plugins: {
-      react: reactPlugin,
+      react: reactPlugin
     },
     settings: {
-      react: { version: "detect" },
+      react: { version: 'detect' }
     },
     rules: {
-      "no-useless-return": "off",
-      "no-var": "off",
-      "object-shorthand": "off",
-      "prefer-const": "off",
-      "react/jsx-boolean-value": "off",
-      "react/jsx-handler-names": "off",
-      "react/prop-types": "off",
-      "react/self-closing-comp": "off",
-      "react-hooks/set-state-in-effect": "off",
-    },
-  },
+      'no-useless-return': 'off',
+      'no-var': 'off',
+      'object-shorthand': 'off',
+      'prefer-const': 'off',
+      'react/jsx-boolean-value': 'off',
+      'react/jsx-handler-names': 'off',
+      'react/prop-types': 'off',
+      'react/self-closing-comp': 'off',
+      'react-hooks/set-state-in-effect': 'off'
+    }
+  }
 ];
